3a411cb36edac7910ff5fdfe8a7c6b20f268fec0
DiegoASC287 <diego.armando020@hotmail.com>
2023-12-14 16:44:22 -0300

    Versão final 7

--- src/app/api/obras/consulta/route.ts
+++ src/app/api/obras/consulta/route.ts
@@ -3,6 +3,7 @@ import prisma from "@/app/lib/prisma"
 
 export async function GET(request: NextRequest) {
     const obras = await prisma.obra.findMany()
+
     return NextResponse.json(obras)
     }
 
