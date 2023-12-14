dd10a95fdc167dee4d801e97c57b39991506076f
DiegoASC287 <diego.armando020@hotmail.com>
2023-12-14 18:01:16 -0300

    Versão final 11

--- src/app/inicio/obras/page.tsx
+++ src/app/inicio/obras/page.tsx
@@ -1,21 +1,24 @@
-'use client'
 import Obra from "@/model/Obra";
-import { useEffect, useState } from "react";
 import ObraItem from "@/components/ObraItem";
 import link from "@/app/pathspers";
-export default function PaginaObras(){
-    const [maquinas, setObras] = useState<Obra[]>()
-    useEffect(() => {
-        fetch(`${link}/api/obras/consulta`, {method: "GET", next: {revalidate: 30}}).then(res => res.json()).then(obrasCarreg => {
-            console.log(obrasCarreg)
-            setObras(obrasCarreg)
-    })}, [])
 
-    function renderizarObras(){
-            return maquinas?.map((e: any) => <ObraItem key={e.id} obra={e}/> )
+async function getObras() {
+    const req = await fetch(`${link}/api/obras/consulta`, {cache: "no-store"})
+    const obras = await req.json()
+    console.log(obras)
+    return obras
+
+}
+
+export default async function PaginaObras(){
+
+    const obras = await getObras()
+
+    function renderizarObras(obras: Obra[]){
+            return obras?.map((e: any) => <ObraItem key={e.id} obra={e}/> )
 
     }
     return (
-        <div className="p-5 flex gap-3">{renderizarObras()}</div>
+        <div className="p-5 flex gap-3">{renderizarObras(obras)}</div>
     )
 }
