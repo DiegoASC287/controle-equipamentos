a54eb4ed47fe4e3b7abd7f55e6afe307ceec035f
DiegoASC287 <diego.armando020@hotmail.com>
2023-12-14 12:41:50 -0300

    Versão final 4

--- src/app/constants/constantes.ts
+++ src/app/constants/constantes.ts
@@ -1,6 +1,6 @@
 const alimentacaoLista = ["", "Diesel", "Gasolina", "Elétrico", "Manual", "Etanol", "Gás"]
 const un = ["Unidade", "mês", 'h', 'km']
-const tipo = ['','Escavadeira hidráulica', "Mini escavadeira", 'Caminhão Munck', 'Caminhão Pipa', 'Pá carregadeira',"Motoniveladora"]
+const tipo = ['','Escavadeira hidráulica', "Mini escavadeira", 'Caminhão Munck', 'Caminhão Pipa', 'Pá carregadeira',"Motoniveladora", "Picape", "Carro de passeio"]
 const categorias = ["", "Pesada", "Leve", "Equipamento eletrônico"]
 const tiposManutencao = ["Preventiva", "Corretiva", "Verificação"]
 
