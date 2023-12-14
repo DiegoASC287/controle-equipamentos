0ee88603b16aa0c0a2882f18ce800d91f3ee6428
DiegoASC287 <diego.armando020@hotmail.com>
2023-12-14 12:02:50 -0300

    Versão final 3

--- src/app/inicio/obras/page.tsx
+++ src/app/inicio/obras/page.tsx
@@ -17,6 +17,6 @@ export default function PaginaObras(){
 
     }
     return (
-        <div className="p-5">{renderizarObras()}</div>
+        <div className="p-5 flex">{renderizarObras()}</div>
     )
 }
