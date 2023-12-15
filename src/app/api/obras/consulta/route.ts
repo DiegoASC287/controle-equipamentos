ad26c2efdcc6e752794b036cd4080fe763eec2fc
DiegoASC287 <diego.armando020@hotmail.com>
2023-12-14 23:13:19 -0300

    Versão final 17

--- src/app/api/obras/consulta/route.ts
+++ src/app/api/obras/consulta/route.ts
@@ -2,11 +2,10 @@ import { NextRequest, NextResponse } from "next/server"
 import prisma from "@/app/lib/prisma"
 
 export async function GET(req: NextRequest) {
-    const {searchParams} = new URL(req.url)
-    const codigo = searchParams.get("t")
-
     const obras = await prisma.obra.findMany()
 
     return NextResponse.json(obras)
     }
+
+export const dynamic = "force-dynamic";
 
